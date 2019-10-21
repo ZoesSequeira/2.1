bd8219c1c720142b1be8bc99adc504c75c883ba9
zcs18 <zcs18@hampshire.edu>
2019-10-20 23:23:47 -0400

    no message

---
+++ Projec t2.1/Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (ControllerScript.activeCube != null) {
+            ControllerScript.activeCube.GetComponent<Renderer>().material.color = Color.white;//turns last cube white
+        }
+        gameObject.GetComponent<Renderer>().material.color = Color.red;//turns current cube red
+        ControllerScript.activeCube = gameObject;//sets currently clicked cube to active
+    }
+}
